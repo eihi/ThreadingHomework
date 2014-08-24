bd23004ae4b7b2addad7488651bf0b8f99e44e53
eihi <etigele@gmail.com>
2014-08-25 01:30:07 +0200

    Added logfile to gitignore
    Conflicts:
    	Webserver/Webserver/Controlserver/logfile.txt

--- Webserver/Webserver/ControlRequestHandler.cs
+++ Webserver/Webserver/ControlRequestHandler.cs
@@ -1,8 +1,7 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-//using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
 using System.Linq;
 using System.Net.Security;
 using System.Net.Sockets;
